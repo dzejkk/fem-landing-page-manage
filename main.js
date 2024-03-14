da2f7714f9d0090b4b615f25ebc51432c2c9ad4b
dzejk <kontrajakub2@gmail.com>
2024-03-14 19:43:00 +0000

    My first Vite Build

---
+++ main.js
@@ -0,0 +1,22 @@
+const primaryHeader = document.querySelector(".primary-header");
+
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
+
+/*SLIDER*/
+
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+  adaptiveHeight: true,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 2500
+});
